de66acacb3b3ccdefd3b6b110f070188862b4109
Joaquin <33739654+joescalona@users.noreply.github.com>
2018-08-12 18:42:46 -0300

    Update ejercicio6.py
    Se añade la posibilidad de que el numero sea negativo o 0.

--- Tarea 1/ejercicio6.py
+++ Tarea 1/ejercicio6.py
@@ -4,19 +4,32 @@
 
 g = 1
 
-	#SOLICITAR AL USUARIO UN VALOR FLOTANTE
-
 x = float(input('Ingresa un valor = '))
+
+#IDEA DE FRANCO SEPULVEDA
+if x<0.0:
+	print('El numero debe ser positivo')
+
+elif x==0.0:
+	print('La raiz de 0 es 0')
 
 	#MIENTRAS QUE LA SIGUIENTE EXPRESION SEA MAYOR A 0.01
 	#MIENTRAS MAS PEQUENO SEA EL MIEMBRO DERECHO MAS PRECISO ES EL RESULTADO
-while abs((g*g)/x-1.)>0.01:
+	#SIN EMBARGO NO SE LA EXPLICACION MATEMATICA DE ESTO
+	
+else:
+	while abs((g*g)/x-1.)>0.01:
 	#g IRA ADQUIRIENDO NUEVOS VALORES
-	g=(g+x/g)/2
+		g=(g+x/g)/2
 
 	#SI LA EXPRESION ANTERIOR ES MENOR AL VALOR ESTABLECIDO ANTES
 	#IMPRIMIR g
-if abs((g*g)/x-1.)<0.01:
-	print ('La raiz de '+str(x)+' es '+str(g))
+	if abs((g*g)/x-1.)<0.01:
+		print ('La raiz de '+str(x)+' es '+str(g))
+
+
+
+
+
 
 
