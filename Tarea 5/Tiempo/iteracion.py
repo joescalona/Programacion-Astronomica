633f88f9c1a6485a9bc682bcd8f960cb7525d712
Joaquin <33739654+joescalona@users.noreply.github.com>
2018-09-14 12:42:13 -0300

    Delete iteracion.py

--- Tarea 5/Tiempo/iteracion.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf-8-*-
-def factorial_iterativo(n):
-    '''Función que recibe un número n y retorna n!
-    '''
-    mul = 1 #primera multiplicación de la iteración
-    for i in range(1,n+1): #itera hasta n
-        mul = mul * i #1*2*3*4*..*n
-    return mul
